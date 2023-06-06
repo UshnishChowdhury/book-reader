7ea500a79802571e6364df6eb7ae662ac01dda87
Ushnish <35863166+UshnishChowdhury@users.noreply.github.com>
2023-06-06 17:57:49 +0530

    Trying out driver.print to get PDF of WebPage

--- src/main/java/com/ushnish/book/reader/MainReader.java
+++ src/main/java/com/ushnish/book/reader/MainReader.java
@@ -1,8 +1,16 @@
 package com.ushnish.book.reader;
 
-import java.net.MalformedURLException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Pdf;
+import org.openqa.selenium.PrintsPage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.print.PrintOptions;
 
 import com.ushnish.book.reader.initialize.ChromeInitializer;
 import com.ushnish.book.reader.load.page.BookPageLoader;
@@ -11,7 +19,7 @@ import com.ushnish.book.reader.utils.PageUtils;
 
 public class MainReader {
 
-	public static void main(String args[]) throws MalformedURLException, InterruptedException {
+	public static void main(String args[]) throws InterruptedException, FileNotFoundException, IOException {
 		ChromeInitializer initializer = new ChromeInitializer();
 		PageLoader pageLoader = new PageLoader();
 		BookPageLoader bookPageLoader = new BookPageLoader();
@@ -20,7 +28,11 @@ public class MainReader {
 		pageLoader.loadPage(requiredDriver);
 		pageLoader.signIn(requiredDriver);
 		bookPageLoader.goToBook(requiredDriver);
-		String pageContent = bookPageLoader.getPageContent(requiredDriver);
+		// String pageContent = bookPageLoader.getPageContent(requiredDriver);
+		Pdf pdf = ((PrintsPage) requiredDriver).print(new PrintOptions());
+		Files.write(Paths.get("./trial.pdf"), OutputType.BYTES.convertFromBase64Png(pdf.getContent()),
+				StandardOpenOption.CREATE);
+		// utils.writeLines(pageContent, "The Handbook of Public Sector Communication");
 		pageLoader.stopDriver(requiredDriver);
 	}
 }
--- src/main/java/com/ushnish/book/reader/utils/PageUtils.java
+++ src/main/java/com/ushnish/book/reader/utils/PageUtils.java
@@ -1,10 +1,34 @@
 package com.ushnish.book.reader.utils;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
 public class PageUtils {
-	
+
 	public String[] spiltText(String content) {
 		String[] lines = content.split(System.lineSeparator());
 		return lines;
+	}
+
+	public void writeLines(String lines, String fileName) throws FileNotFoundException, IOException {
+		fileName = fileName.concat(".docx");
+		try (XWPFDocument doc = new XWPFDocument()) {
+			XWPFParagraph paragraph = doc.createParagraph();
+			paragraph.setAlignment(ParagraphAlignment.LEFT);
+			XWPFRun runLines = paragraph.createRun();
+			runLines.setFontSize(12);
+			runLines.setFontFamily("New Roman");
+			runLines.setText(lines);
+			try (FileOutputStream out = new FileOutputStream(fileName)) {
+				doc.write(out);
+			}
+		}
 	}
 
 }
